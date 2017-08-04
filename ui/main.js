24f98be9d4dcb071a3c2a51a762a60f34f14cc7b
pulkit151 <30702599+pulkit151@users.noreply.github.com>
2017-08-05 00:26:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,17 @@
-console.log('Loaded!');
-<;"Pls help me to sort out this whole issue" >
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+            Hello Everyone & Start a new Journey.
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
