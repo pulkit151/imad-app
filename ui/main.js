c3960b3fc7a9a7aeff1452b638588f302cf5d0dd
pulkit151 <30702599+pulkit151@users.noreply.github.com>
2017-08-06 14:58:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@
         </div>
         <br>
         <div class="center text-big bold">
-            Hello Everyone & Start a new Journey with Pulkit
+            Hello Everyone & Start a new Journey with me and have a fun in this.
         </div>
         <script type="text/javascript" src="/ui/main.js">
         </script>
