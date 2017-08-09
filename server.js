5dc85dafc6e76b26f4ba5c729243aab39488ea5b
pulkit151 <30702599+pulkit151@users.noreply.github.com>
2017-08-09 23:11:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
- var express = require('express');
- var morgan = require('morgan');
- var path = require('patch');
+  var express = require('express');
+  var morgan = require('morgan');
+  var path = require('patch');
  
   var app = express();
   app.use(morgan('combined'));
  
   app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'pulkit123patil@gmail.com', 'index.html'));
- });
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+   });
  
- app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'pulkit123patil@gmail.com', 'style.css'));
+   app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
  });
  
  app.get('/ui/madi.png', function (req, res) {
-   res.sendFile(path.join(__dirname, 'pulkit123patil@gmail.com', 'madi.png'));
+   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
  });
  
  
- // Do not change port, otherwise your app won't run on IMAD servers
- // Use 8080 only for local development if you already have apache running on 80
- 
- var port = 8080;
+ var port = 8080; // Use 8080 only for local development if you already have apache running on 80
  app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
  });
+ 
