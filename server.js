19d37b8eb0ad1605cfa6e223d03b28de857a3e01
pulkit151 <30702599+pulkit151@users.noreply.github.com>
2017-08-12 18:14:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@
   var path = require('patch');
  
   var app = express();
-  app.use(morgan('combined'));
+   app.use(morgan('combined'));
  
   app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-   });
+  });
  
    app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
  });
  
  app.get('/ui/madi.png', function (req, res) {
@@ -18,8 +18,8 @@
  });
  
  
- var port = 80; // Use 8080 only for local development if you already have apache running on 80
- app.listen(80, function () {
+ var port = 8080; // Use 8080 only for local development if you already have apache running on 80
+ app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
  });
  
